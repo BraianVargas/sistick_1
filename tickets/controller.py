11edeb778d0ac77b0d8fb82a1ea41cbbcae14a8b
Braian Vargas <bvargas@energiasanjuan.com.ar>
2023-03-31 17:00:40 -0300

    [FIX] general bugs... [ADD] edit and delete with modals confirmation

--- tickets/controller.py
+++ tickets/controller.py
@@ -5,21 +5,10 @@ from extensions import db
 from tickets.model import Ticket
 from usuarios.model import *
 
-
-
 def getAllTicketsController():
-     data = []
-     i = 0
      ticketList = db.session.query(Ticket).all()
-     for ticket in ticketList:
-          creator = db.session.query(Usuario).filter_by(id=ticket.creator_id).first()
-          assigned = db.session.query(Usuario).filter_by(id=ticket.assigned_to).first()
-          ticket.creator_id = creator.username          
-          ticket.assigned_to = assigned.username
-          
-          data.append(ticket)
-
-     return data
+     
+     return ticketList
 
 def createTicketController(data):
      try:
@@ -27,7 +16,7 @@ def createTicketController(data):
           assigned= db.session.query(Usuario).filter_by(username = data["assigned_to"].lower()).first()
           
           ticket = Ticket(
-               data["title"],
+               data["title"], 
                data["description"],
                creator.id,
                assigned.id,
@@ -43,6 +32,22 @@ def createTicketController(data):
           flash(f"Error al cargar ticket. {e}")
           return redirect(url_for("ticket_blueprint.newTicket"))
 
-def editTicketController(ticketId):
-     ticket = db.session.query(Ticket).filter_by(id = ticketId).first()
-     return ""
+def editTicketController(requestedData, ticketId):
+    try:
+        # Obtener el ticket a editar
+        ticket = db.session.query(Ticket).filter_by(id=ticketId).first()
+
+        # Actualizar los campos del ticket con los datos enviados en el formulario
+        ticket.title = requestedData['title']
+        ticket.description = requestedData['description']
+        ticket.assigned_to = requestedData['assigned_to']
+        ticket.state = requestedData['state']
+
+        # Guardar los cambios en la base de datos
+        db.session.commit()
+        flash(f"Ticket actualizado correctamente. Id: {ticket.id} ")
+        return 
+    except Exception as e:
+        flash(f"Error en actualización de ticket. \n {e}")
+        return 
+
--- tickets/routes.py
+++ tickets/routes.py
@@ -9,27 +9,52 @@ from tickets.controller import *
 def index():
      if session.get("username") != None:
           data = getAllTicketsController()
-          return render_template("ticket/index.html", data = data, logged=True, admin=session["admin"],pagination="")
+          users = db.session.query(Usuario).order_by(Usuario.id.desc()).all()
+          return render_template("ticket/index.html", data = data, logged=True, admin=session["admin"], users = users)
      else:
           return redirect(url_for("user_blueprint.login"))
 
 
 @ticketsBP.route("/newTicket",methods=["GET","POST"])
 def newTicket():
-     users = db.session.query(Usuario).all()
-     if request.method == "GET":
-          return render_template("ticket/newTicketForm.html", logged = True, admin=session["admin"], users = users)
+     if session.get("username") != None:
+          users = db.session.query(Usuario).all()
+          if request.method == "GET":
+               return render_template("ticket/newTicketForm.html", logged = True, admin=session["admin"], users = users)
+          else:
+               data = request.form.to_dict()
+               createTicketController(data)
+               return render_template("ticket/newTicketForm.html", logged = True, admin=session["admin"], users = users)
      else:
-          data = request.form.to_dict()
-          createTicketController(data)
-          return render_template("ticket/newTicketForm.html", logged = True, admin=session["admin"], users = users)
-     
+          return redirect(url_for("user_blueprint.login"))
+
 @ticketsBP.route("/edit/<int:ticketId>", methods=["GET","POST"])
 def editTicket(ticketId):
-     editTicketController(ticketId)
-     return redirect(url_for("ticket_blueprint.index"))
-     
+     if session.get("username") != None:
+          if request.method == "GET":
+               users = db.session.query(Usuario).all()
+               ticket = db.session.query(Ticket).filter_by(id=ticketId).first()
+               return render_template("ticket/edit_ticket.html", logged = True, ticket=ticket, users =users)
+          else:
+               data = request.form.to_dict()
+               editTicketController(data, ticketId)
+               return redirect(url_for("ticket_blueprint.index"))
+     else:
+          return redirect(url_for("user_blueprint.login"))
+
 @ticketsBP.route("/delete/<int:ticketId>", methods=["GET","POST"])
 def deleteTicket(ticketId):
-     print(ticketId)
-     return redirect(url_for("ticket_blueprint.index"))
+     if session.get("username") != None:
+          users = db.session.query(Usuario).all()    
+          try:
+               ticket = db.session.query(Ticket).filter_by(id=ticketId).first()
+               # Delete the ticket from the database
+               db.session.delete(ticket)
+               db.session.commit()
+               flash(f"Ticket eliminado correctamente. Id: {ticket.id} ")
+               return redirect(url_for("ticket_blueprint.index"))
+          except Exception as e:
+               flash(f"Error en actualización de ticket. \n {e}")
+               return
+     else:
+          return redirect(url_for("user_blueprint.login"))
